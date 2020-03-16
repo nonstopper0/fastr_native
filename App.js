ae6cbd08e01c37bb65ebaeffb3ae01ad927ee581
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-03-16 15:25:23 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text>Fastr</Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    position: 'absolute',
+    top: 45,
+    backgroundColor: 'black',
+
+  }
 });
